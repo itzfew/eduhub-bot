25b3648d0b616f49db703f8c010f6e68522f227f
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-01-18 20:31:23 +0530

    Update index.ts

--- src/text/index.ts
+++ src/text/index.ts
@@ -1,3 +1,4 @@
 export * from './greeting';
 export * from './pyq';
 export * from './calculator';  // Updated export to use calculator instead of groupCommands
+export * from './pdf';  // Added export for pdf
