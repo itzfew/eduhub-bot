ca0ae8d1b26bf1c4043c5460364788d96e03a556
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-01-20 15:34:51 +0530

    Update greeting.ts

--- src/text/greeting.ts
+++ src/text/greeting.ts
@@ -58,34 +58,4 @@ Hey ${userName}, how may I assist you today? Choose an option below:
   }
 };
 
-// Handle the callback from the inline buttons
-const handleCallback = () => async (ctx: Context) => {
-  const callbackData = ctx.callbackQuery?.data;
-
-  if (callbackData) {
-    switch (callbackData) {
-      case 'pyqs':
-        await ctx.reply(`Please select the following command (e.g., /pyq or /exam) for NEET and JEE PyQs.`);
-        break;
-      case 'study_material':
-        await ctx.reply('To access study materials, use the /study command.');
-        break;
-      case 'commands':
-        await ctx.reply('Here are the available commands: /about, /help, /study, /neet, /jee');
-        break;
-      case 'about':
-        await ctx.reply('This bot helps you with NEET, JEE, and other study materials. It also provides quick access to previous years’ questions, calculators, and more.');
-        break;
-      case 'help':
-        await ctx.reply('Here is the help section. You can use these commands: /about, /help, /study, /pyq, /exam.');
-        break;
-      default:
-        await ctx.reply(`Sorry, I don't recognize this option.`);
-    }
-
-    // Acknowledge the callback to remove the button (optional)
-    await ctx.answerCbQuery();
-  }
-};
-
-export { greeting, handleCallback };
+export { greeting };
